15ad631bdaa00e761c009dc86dca9b0ca08db02d
Hyperion <ossi.tammi@gmail.com>
2020-06-11 22:05:41 +0300

    Added locale to navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 import Language from './Language'
+import { IntlContextConsumer } from 'gatsby-plugin-intl'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -35,66 +36,70 @@ const Navbar = class extends React.Component {
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+      <IntlContextConsumer>
+        {({ languages, language: currentLocale }) =>
+          <nav
+            className="navbar is-transparent"
+            role="navigation"
+            aria-label="main-navigation"
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <div>
-                <Language/>
+            <div className="container">
+              <div className="navbar-brand">
+                <Link to={`/${currentLocale}`} className="navbar-item" title="Logo">
+                  <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+                </Link>
+                {/* Hamburger menu */}
+                <div
+                  className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                  data-target="navMenu"
+                  onClick={() => this.toggleHamburger()}
+                >
+                  <span />
+                  <span />
+                  <span />
+                </div>
               </div>
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
+              <div
+                id="navMenu"
+                className={`navbar-menu ${this.state.navBarActiveClass}`}
               >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+                <div className="navbar-start has-text-centered">
+                  <Link className="navbar-item" to={`/${currentLocale}/about`}>
+                    About
+                  </Link>
+                  <Link className="navbar-item" to={`/${currentLocale}/products`}>
+                    Products
+                  </Link>
+                  <Link className="navbar-item" to={`/${currentLocale}/blog`}>
+                    Blog
+                  </Link>
+                  <Link className="navbar-item" to={`/${currentLocale}/contact`}>
+                    Contact
+                  </Link>
+                  <Link className="navbar-item" to="/contact/examples">
+                    Form Examples
+                  </Link>
+                </div>
+                <div className="navbar-end has-text-centered">
+                  <div>
+                    <Language/>
+                  </div>
+                  <a
+                    className="navbar-item"
+                    href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon">
+                      <img src={github} alt="Github" />
+                    </span>
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </nav>
+          </nav>
+        }
+      </IntlContextConsumer>
     )
   }
 }
