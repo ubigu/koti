2dee9181bfcce65fbe139cab6bc1a55c89117563
Ilpo Tammi <ilpo@ubigu.fi>
2020-08-14 14:00:28 +0300

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,36 +1,30 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const FeatureGrid = ({ gridItems }) => (
-  <div className="columns is-multiline">
-    {gridItems.map((item) => (
-      <div key={item.text} className="column is-6">
-        <section className="section">
-          <div className="has-text-centered">
-            <div
-              style={{
-                width: '240px',
-                display: 'inline-block',
-              }}
-            >
-              <PreviewCompatibleImage imageInfo={item} />
-            </div>
-          </div>
-          <p>{item.text}</p>
-        </section>
-      </div>
-    ))}
-  </div>
-)
+const useStyles = makeStyles(theme => {
+  return {
+    gridItem: {
+      display: 'flex',
+      justifyContent: 'center',
+      position: 'relative'
+    },
+  }
+})
 
-FeatureGrid.propTypes = {
-  gridItems: PropTypes.arrayOf(
-    PropTypes.shape({
-      image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-      text: PropTypes.string,
-    })
-  ),
+const FeatureGrid = () => {
+
+  const classes = useStyles()
+
+  return (
+    <Grid container justify='center' style={{height: 400}}>
+      <Grid item xs={12} md={4} className={classes.gridItem}>
+      </Grid>
+      <Grid item xs={12} md={4}>Jaska</Grid>
+      <Grid item xs={12} md={4}>Teppo</Grid>
+    </Grid>
+  )
+
 }
 
 export default FeatureGrid
