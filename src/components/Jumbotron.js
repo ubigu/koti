f2003771556079e6522295a948b301b46961ea68
Ilpo Tammi <ilpo@ubigu.fi>
2020-08-14 13:59:31 +0300

    Update Jumbotron.js

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,55 +3,92 @@ import bgVideo from "./../img/bg.mp4";
 import logo from '../img/ubigu_logo_white.svg';
 import { IntlContextConsumer, useIntl } from 'gatsby-plugin-intl'
 
+/** MUI */
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Hidden } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => {
+    return {
+        jumbotronContainer: {
+            width: '100vw',
+            height: 'calc(50vh - 52px)',
+            [theme.breakpoints.down('sm')]: {
+                height: 'calc(100vh - 52px)'
+            },
+            display: 'flex',
+            overflow: 'hidden',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            userSelect: 'none',
+            flexDirection: 'column',
+        },
+        jumboTwo: {
+            position: 'absolute',
+            top: 52
+        },
+        video: {
+            objectFit: 'cover',
+            width: '100vw',
+            height: "100%",
+            position: 'relative'
+        },
+        img: {
+            height: 100,
+            [theme.breakpoints.down('sm')]: {
+                height: 75
+            }
+        },
+        typography: {
+            color: '#FFF',
+            padding: '0.25em',
+            fontFamily: "titillium web !important",
+            marginTop: theme.spacing(1)
+        },
+        span: {
+            width: '100vw',
+            display: 'flex',
+            height: 400,
+            top: 52,
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+        },
+    }
+})
+
 const Jumbotron = () => {
 
-    const intl = useIntl();
+    const intl = useIntl()
+    const classes = useStyles()
 
     return (
         <IntlContextConsumer>
             {({ languages, language: currentLocale }) =>
-                <div
-                    className="full-width-image margin-top-0"
-                    style={{
-                        display: 'flex',
-                        height: 400,
-                        overflow: 'hidden',
-                        justifyContent: 'space-around',
-                        alignItems: 'center',
-                        userSelect: 'none',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <video autoPlay muted loop
-                        style={{ objectFit: 'cover', width: '100vw', height: "400px", position: 'absolute', borderRadius: 1, top: 52 }}>
-                        <source src={bgVideo} type="video/mp4"></source>
-                    </video>
-                    <span className='gradient' style={{
-                        position: 'absolute',
-                        width: '100vw',
-                        display: 'flex',
-                        height: 400,
-                        top: 52,
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }}
-                    >
-                        <img
-                            src={logo}
-                            alt="Ubigu"
-                            style={{ height: 100 }}
-                        />
-                        <h3
-                            className="is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-                            style={{
-                                color: '#FFF',
-                                padding: '0.25em',
-                            }}
-                        >
-                            {intl.formatMessage({ id: "motto" })}
-                        </h3>
-                    </span>
+                <div className={classes.container}>
+                    <div className={classes.jumbotronContainer}>
+                        <video autoPlay muted loop className={classes.video}>
+                            <source src={bgVideo} type="video/mp4"></source>
+                        </video>
+                    </div>
+                    <div className={`${classes.jumbotronContainer} ${classes.jumboTwo}`}>
+                        <span className={classes.span}>
+                            <img
+                                src={logo}
+                                alt="Ubigu"
+                                className={classes.img}
+                            />
+                            <Hidden mdUp>
+                                <Typography variant='subtitle1' className={classes.typography} align='center'>
+                                    {intl.formatMessage({ id: "motto" })}
+                                </Typography>
+                            </Hidden>
+                            <Hidden smDown>
+                                <Typography variant='h6' className={`${classes.typography} ${classes.typography2}`} align='center'>
+                                    {intl.formatMessage({ id: "motto" })}
+                                </Typography>
+                            </Hidden>
+                        </span>
+                    </div>
                 </div>
             }
         </IntlContextConsumer>
