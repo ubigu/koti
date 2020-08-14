377ded3017f5aa2a313e113b1801ae06e37bcd13
Ilpo Tammi <ilpo@ubigu.fi>
2020-08-14 13:59:54 +0300

    Updated theming / styling

---
+++ src/components/theme.js
@@ -0,0 +1,17 @@
+/**
+ * MUI theme
+ * @see https://material-ui.com/customization/theming/
+ */
+
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#00b4a2",
+        },
+        type: 'light',
+    }
+});
+
+export default theme;
